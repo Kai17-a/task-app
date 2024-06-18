fc8d4fe9a677e1c7a6283425e69bebcbd9b215c5
Kai17-a <k.s25skt@gmail.com>
2024-06-19 01:01:25 +0900

    feat: ワークスペース一覧をDBから取得

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,20 +2,31 @@
   <v-app>
     <v-navigation-drawer permanent rail>
       <v-list density="compact" nav color="primary">
-        <v-list-item :active="true" prepend-icon="mdi-home" value="home" @click="navigateTo('/')">
+        <v-list-item :active="checkPath('/')" prepend-icon="mdi-home" value="home" @click="navigateTo('/')">
           <v-tooltip activator="parent" location="start">ホーム</v-tooltip>
         </v-list-item>
         <v-divider class="mb-1"></v-divider>
-        <v-list-item v-for="workspace in workspaces" :key="workspace.id" :value="workspace.title" @click="navigateTo(`/workspaces/${workspace.id}`)">
+
+        <!-- ワークスペースリスト -->
+        <v-list-item v-for="workspace in workspaces"
+          :active="checkPath(`/workspaces/${workspace.id}`)"
+          :key="workspace.id"
+          :value="workspace.name"
+          @click="navigateTo(`/workspaces/${workspace.id}`)"
+        >
           <template v-slot:prepend>
-            <v-icon :icon="workspace.icon"></v-icon>
+            <v-icon icon="mdi-book"></v-icon>
           </template>
-          <v-tooltip activator="parent" location="start">ワークスペース追加</v-tooltip>
+          <v-tooltip activator="parent" location="start">{{ workspace.name }}</v-tooltip>
         </v-list-item>
+
+        <!-- ワークスペース追加 -->
         <v-list-item prepend-icon="mdi-plus-circle-outline" value="add">
           <v-tooltip activator="parent" location="start">ワークスペース追加</v-tooltip>
         </v-list-item>
         <v-divider class="mb-1"></v-divider>
+
+        <!-- 設定 -->
         <v-list-item prepend-icon="mdi-cog" value="setting">
           <v-tooltip activator="parent" location="start">設定</v-tooltip>
         </v-list-item>
@@ -27,32 +38,22 @@
   </v-app>
 </template>
 <script lang="ts" setup>
-const workspaces: object[] = [
-  {
-    id: 1,
-    title: "title1",
-    descript: "説明",
-    icon: "mdi-arrow-up"
-  },
-  {
-    id: 2,
-    title: "title2",
-    descript: "説明",
-    icon: "mdi-arrow-down"
-  },
-  {
-    id: 3,
-    title: "title3",
-    descript: "説明",
-    icon: "mdi-arrow-right"
-  },
-  {
-    id: 4,
-    title: "title4",
-    descript: "説明",
-    icon: "mdi-arrow-left"
-  },
-]
+import Database from "tauri-plugin-sql-api"
+
+interface WorkSpace {
+  id: number;
+  name: string;
+}
+
+const db = await Database.load("sqlite:task_app.db")
+// ワークスペース取得
+const workspaces: WorkSpace[] = await db.select("SELECT id, name FROM workspaces")
+
+function checkPath(path: string): boolean {
+  const route = useRoute()
+  const currentPath = route.path
+  return currentPath === path
+}
 </script>
 <style scoped>
 .link {
