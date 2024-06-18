97d17cd94c8666ada90fc6f8e706937e1a43c2b7
Kai17-a <k.s25skt@gmail.com>
2024-06-19 02:29:30 +0900

    feat: 登録機能追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,7 +16,7 @@
             @click="navigateTo(`/workspaces/${workspace.id}`)"
           >
             <div v-for="status in workspace.statuses" class="pl-3">
-              <v-chip v-if="status.status !== null" density="compact" variant="flat" :color="getStatusColor(status.status)">
+              <v-chip v-if="status.status !== null" density="compact" variant="flat" :color="getStatusColor(status.status)" class="mb-1">
                 {{ getStatusName(status.status) }}: {{ status.count }}
               </v-chip>
               <div class="ml-2 mt-1 text-disabled" v-else>タスクなし</div>
@@ -31,16 +31,63 @@
             min-height="150"
             max-width="200"
             hover
+            @click="registWorkspaceDialog = true"
           >
             <div style="margin: auto;">
               <p class="text-h5 text-disabled text-center">
                 追加
               </p>
               <div class="d-flex justify-center">
-                <v-btn class="ma-2" size="small" color="grey-lighten-5" icon="mdi-plus"></v-btn>
+                <v-icon class="ma-2" size="large" color="grey-lighten-1" icon="mdi-plus-circle-outline"></v-icon>
               </div>
             </div>
           </v-card>
+          <v-dialog v-model="registWorkspaceDialog" width="600">
+            <v-card title="タスク追加">
+              <template v-slot:append>
+                <v-icon icon="mdi-close" :class="(registPending) ? 'not-allowed' : ''" @click="registWorkspaceDialog = false"></v-icon>
+              </template>
+
+              <v-form @submit.prevent>
+                <div class="ml-10 mr-10">
+                  <div class="text-right">
+                    {{ newWorkspaceName.length }}&nbsp;/&nbsp;15
+                  </div>
+                  <v-text-field
+                    v-model="newWorkspaceName"
+                    :rules="rules"
+                    label="ワークスペース名"
+                    density="compact"
+                    variant="outlined"
+                    maxlength="15"
+                    class="mb-3"
+                  />
+                  <div class="text-right">
+                    {{ newWorkspaceDescript.length }}&nbsp;/&nbsp;15
+                  </div>
+                  <v-textarea label="説明" variant="outlined" rows="1" v-model="newWorkspaceDescript" />
+                </div>
+                <v-card-actions max-width="300">
+                  <v-spacer></v-spacer>
+                  <v-btn type="submit" variant="flat" color="primary" text="ワークスペース追加" @click="addWorkSpace" />
+                </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="registErrDialog" width="450">
+            <v-card title="エラー" color="red-lighten-5">
+              <template v-slot:append>
+                <v-icon icon="mdi-close" @click="registErrDialog = false"></v-icon>
+              </template>
+              <v-card-text max-width="auto">
+                <p>登録に失敗しました。</p>
+                以下の確認をしてください。
+                <li>ワークスペース名が重複していない</li>
+                <br>
+                <p>それ以外の場合は開発者に聞いてね(/・ω・)/</p>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
         </v-col>
       </v-row>
     </div>
@@ -50,6 +97,9 @@
 </template>
 <script lang="ts" setup>
 import Database from "tauri-plugin-sql-api"
+
+const registWorkspaceDialog: Ref<boolean> = ref(false)
+const registPending: Ref<boolean> = ref(false)
 
 interface WorkSpace {
   id: number;
@@ -71,8 +121,7 @@ const db = await Database.load("sqlite:task_app.db")
 
 const aggregatedData: Ref<AggregatedTask[]> = ref([])
 const workspacesSize: Ref<number> = ref(0)
-
-  getWorkspaces().then((result) => console.log(typeof result))
+await getWorkspaces()
 
 async function getWorkspaces(): Promise<void> {
   // ワークスペース取得
@@ -154,6 +203,30 @@ function getStatusColor(status: string): string {
     default:
       return "gray";
   }
+}
+
+const newWorkspaceName: Ref<string> = ref("")
+const newWorkspaceDescript: Ref<string> = ref("")
+const registErrDialog: Ref<boolean> = ref(false)
+const rules = [
+  (value: string): boolean | string => {
+    if (value != "") return true
+    return "ワークスペース名は必須です"
+  }
+]
+async function addWorkSpace() {
+  if (!newWorkspaceName.value) return
+
+  registPending.value = true
+  await db.execute(
+    "INSERT into workspaces (name, descript) VALUES ($1, $2)",
+    [newWorkspaceName.value, newWorkspaceDescript.value]
+  ).catch((err) => {
+    registErrDialog.value = true
+  })
+  await getWorkspaces()
+  registPending.value = false
+  registWorkspaceDialog.value = false
 }
 </script>
 <style scoped>
